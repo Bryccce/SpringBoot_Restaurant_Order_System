9f99d8ee74afa7c939d83293718625da4cc666b6
bryccce <qy765@nyu.edu>
2025-02-03 23:34:21 -0500

    update

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -5,11 +5,15 @@ import com.sky.dto.DishPageQueryDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import software.amazon.awssdk.services.s3.endpoints.internal.Value;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -30,6 +34,24 @@ public class DishController {
     public Result<PageResult> page(DishPageQueryDTO dto){
         PageResult pageResult = dishService.pageQuery(dto);
         return Result.success(pageResult);
+    }
+
+    @DeleteMapping
+    public Result delete(@RequestParam List<Long> ids){
+        dishService.deleteBatch(ids);
+        return Result.success();
+    }
+
+    @GetMapping("/{id}")
+    public Result<DishVO> getById(@PathVariable Long id){
+        DishVO dishVO = dishService.getByIdWithFlavor(id);
+        return Result.success(dishVO);
+    }
+
+    @PutMapping
+    public Result update(@RequestBody DishDTO dishDTO){
+        dishService.updateWIthFlavor(dishDTO);
+        return Result.success();
     }
 
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -3,10 +3,19 @@ package com.sky.service;
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
+
+import java.util.List;
 
 
 public interface DishService {
     public void saveWithFlavor(DishDTO dto);
 
     PageResult pageQuery(DishPageQueryDTO dto);
+
+    void deleteBatch(List<Long> ids);
+
+    DishVO getByIdWithFlavor(Long id);
+
+    void updateWIthFlavor(DishDTO dishDTO);
 }
